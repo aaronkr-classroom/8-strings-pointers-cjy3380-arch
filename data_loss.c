6dc3459102cd2c2d16775ac322238c2898c51fbd
cjy <cjy3380@gmail.com>
2025-11-04 16:58:40 +0900

    finish

---
+++ data_loss.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	unsigned int x = 0x12345678;
+	long lg = 2147483648;
+	double xl = 123456789123.456789;
+
+	printf("Uint: %d\n", x);
+	printf("Uint -> short: %d\n", (short)x);
+	printf("Uint -> char: %d\n", (char)x);
+
+	printf("Uint: %d\n", lg);
+	printf("Uint -> int: %d\n", (int)lg);
+	printf("Uint -> char: %d\n", (char)lg);
+
+	printf("Uint: %lf\n", xl);
+	printf("Uint -> long: %d\n", (long)xl);
+	printf("Uint -> short: %d\n", (short)xl);
+
+	return 0;
+}
---
+++ endian.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+void check_endian() {
+	unsigned int num = 0x12345678;
+	unsigned char* byte_ptr = (unsigned char*)&num;
+
+	for (int i = 0; i < sizeof(num); i++) {
+		printf("%p 0x%02x\n", (byte_ptr + i), (byte_ptr[i]));
+	}
+
+	if (byte_ptr[0] == 0x12) {
+		printf("\nThis is a big endian");
+	}
+
+	else if (byte_ptr[0] == 0x78) {
+		printf("\nThis is_little_endian...");
+	}
+	else {
+		printf("\nUnknown endian! ㅠㅠ");
+	}
+}
+
+int main(void) {
+	check_endian();
+	return 0;
+}
